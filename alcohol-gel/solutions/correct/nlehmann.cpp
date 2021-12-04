2c9fd04f2e676a01cabfdb214600ee9bfa8b903d
Nico Lehmann <nico.lehmannm@gmail.com>
2021-12-03 18:07:53 -0800

    alcohol-gel: solución c++

---
+++ alcohol-gel/solutions/correct/nlehmann.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  int g = 0, j = 0, max_g = 0, max_j = 0, max_k = 0;
+  for (int i = 1; i <= n; ++i) {
+    int ci, pi, ki, vi;
+    scanf("%d%d%d%d", &ci, &pi, &ki, &vi);
+    int b = ki * vi - ci * pi;
+    if (g + b > 0) {
+      g += b;
+
+      if (g > max_g || (g == max_g && i - j > max_k - max_j)) {
+        max_g = g;
+        max_j = j + 1;
+        max_k = i;
+      }
+    } else {
+      g = 0;
+      j = i;
+    }
+  }
+
+  if (max_g > 0) {
+    printf("%d %d %d\n", max_j, max_k, max_g);
+  } else {
+    printf("IMPOSIBLE\n");
+  }
+}
--- alcohol-gel/solutions/correct/sol_bernardo.py
+++ alcohol-gel/solutions/correct/sol_bernardo.py
@@ -5,19 +5,19 @@ ta, tb = 0, 0
 cs = 0
 ca = 0
 for i in range(n):
-	c, p, k, v = map(int, input().split(" "))
-	daily_diff = k*v - c*p
-	if cs + daily_diff > 0:
-		cs += daily_diff
-	else:
-		cs = 0
-		ca = i+1
-	if cs > s or (cs == s and i-ca > tb-ta):
-		s = cs
-		ta = ca
-		tb = i
-		
+    c, p, k, v = map(int, input().split(" "))
+    daily_diff = k * v - c * p
+    if cs + daily_diff > 0:
+        cs += daily_diff
+    else:
+        cs = 0
+        ca = i + 1
+    if cs > s or (cs == s and i - ca > tb - ta):
+        s = cs
+        ta = ca
+        tb = i
+
 if s == 0:
-	print("IMPOSIBLE")
+    print("IMPOSIBLE")
 else:
-	print(f"{ta+1} {tb+1} {s}")
+    print(f"{ta+1} {tb+1} {s}")
