f76a088844007ce23b75e7c06b870874a7555085
Rodrigo Alonso <rodrigo.alonso.o@gmail.com>
2021-12-04 21:32:55 -0300

    terreno: validadores

---
+++ terreno/attic/validators/st2.cpp
@@ -0,0 +1,21 @@
+#include "testlib.h"
+
+using namespace std;
+
+int main() {
+  registerValidation();
+
+  int M = inf.readInt(1, 1000, "Rows");
+  inf.readSpace();
+  int N = inf.readInt(1, 1000, "Columns");
+  inf.readEoln();
+
+  for (int i = 1; i <= M; ++i) {
+    for (int j = 1; j <= N; ++j) {
+      int A = inf.readInt(1, 1000000000, "Height");
+      if (j != N) inf.readSpace();
+    }
+    inf.readEoln();
+  }
+  inf.readEof();
+}
