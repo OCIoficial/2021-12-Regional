8b310c51fcf8a4cc5e1c3aa4d109f41a2740b30b
Rodrigo Alonso <rodrigo.alonso.o@gmail.com>
2021-12-10 16:09:52 -0300

    segundo: actualizar generación de casos y testplan

--- segundo/attic/random_choices.py
+++ segundo/attic/random_choices.py
@@ -6,11 +6,12 @@ random.seed(str(sys.argv[1:]))
 N = int(sys.argv[2])
 M = int(sys.argv[3])
 
-assert 0 < M <= 1000000000
+assert 0 < M <= N
 
 print(N)
 
 elements = random.sample(range(1, 1000000001), M)
-elements = random.choices(elements, k=N)
+elements.extend(random.choices(elements, k=N-M))
+random.shuffle(elements)
 
 print(' '.join(str(i) for i in elements))
